1a18a3c3cf260f968b704d2a45d3343d48992257
ForRest1217 <Jihan2519@naver.com>
2025-07-06 22:29:54 +0900

    bullet

--- Assets/00. Work/Scripts/Systems/Bullet.cs
+++ Assets/00. Work/Scripts/Systems/Bullet.cs
@@ -9,7 +9,7 @@ public class Bullet : MonoBehaviour
     private float angle = 0;
 
     [SerializeField] private bool bounce = false;
-
+    [SerializeField] private bool penetration = false;
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -25,9 +25,8 @@ public class Bullet : MonoBehaviour
 
         if (bounce)
             Physics2D.IgnoreLayerCollision(6, 7, false);
-        else
+        else if (penetration)
             Physics2D.IgnoreLayerCollision(6, 7, true);
-
     }
 
     void Update()
@@ -43,14 +42,20 @@ public class Bullet : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("Wall"))
         {
-            collision.gameObject.GetComponent<BoxCollider2D>().enabled = true;
-            Vector2 normal = collision.contacts[0].normal;
-            mousedirection = Vector2.Reflect(mousedirection, normal).normalized;
-
-            float angle = Mathf.Atan2(mousedirection.y, mousedirection.x) * Mathf.Rad2Deg;
-            rigid.angularVelocity = 0f;
-            rigid.rotation = angle;
-            transform.rotation = Quaternion.Euler(0, 0, angle);
+            if (bounce)
+            {
+                collision.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+                Vector2 normal = collision.contacts[0].normal;
+                mousedirection = Vector2.Reflect(mousedirection, normal).normalized;
+                float angle = Mathf.Atan2(mousedirection.y, mousedirection.x) * Mathf.Rad2Deg;
+                rigid.angularVelocity = 0f;
+                rigid.rotation = angle;
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
         }
         else if (collision.gameObject.CompareTag("DeadZone"))
         {
