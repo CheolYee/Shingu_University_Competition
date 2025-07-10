0d998254b4f2762d67e3d56d937cec819e10adc8
ForRest1217 <Jihan2519@naver.com>
2025-07-11 00:29:51 +0900

    JJH_enemyAction

--- Assets/00. Work/02. Scripts/Systems/Enemy.cs
+++ Assets/00. Work/02. Scripts/Systems/Enemy.cs
@@ -1,10 +1,16 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public event Action OnDead;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
+        {
+            OnDead?.Invoke();
             Destroy(gameObject);
+        }
     }
 }
