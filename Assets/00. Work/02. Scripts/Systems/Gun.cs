660291ca9ef94d93e7cc1ac4f0c980d723c7aea0
ForRest1217 <Jihan2519@naver.com>
2025-08-01 18:12:59 +0900

    Pulling

--- Assets/00. Work/02. Scripts/Systems/Gun.cs
+++ Assets/00. Work/02. Scripts/Systems/Gun.cs
@@ -1,28 +1,27 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private int poolSize = 10;
-    private List<GameObject> bulletPool = new List<GameObject>();
+    private GameObject[] bulletPool;
     private bool canfire = true;
     private Vector3 mouse;
 
     private void Start()
     {
-        for (int i = 0; i < poolSize; i++)
-        {
-            GameObject bullet = Instantiate(bulletPrefab);
-            bullet.SetActive(false);
-            bulletPool.Add(bullet);
-        }
+        bulletPool = new GameObject[1];
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        bulletPool[0] = bullet;
+        bullet.SetActive(false);
     }
 
     private void Update()
     {
+        if (Time.timeScale == 0f)
+            return;
+
         LookAtMouse();
     }
 
@@ -44,38 +43,14 @@ public class Gun : MonoBehaviour
 
     private void Shot()
     {
-        GameObject bullet = GetInactiveBulletFromPool();
-        if (bullet == null)
-        {
+        GameObject bullet = bulletPool[0];
+        if (bullet.activeSelf || !canfire)
             return;
-        }
 
         bullet.transform.position = transform.position;
-        bullet.transform.rotation = transform.rotation;
-
-        Bullet bulletScript = bullet.GetComponent<Bullet>();
-
-        ReadyButton readyBtn = FindFirstObjectByType<ReadyButton>();
-        if (readyBtn != null && readyBtn.cardSequence.Count > 0)
-        {
-            bulletScript.SetCardSequence(readyBtn.cardSequence);
-        }
-
         bullet.SetActive(true);
-        bulletScript.Initialize();
-
         canfire = false;
         StartCoroutine(CoolTime());
-    }
-
-    private GameObject GetInactiveBulletFromPool()
-    {
-        foreach (GameObject bullet in bulletPool)
-        {
-            if (!bullet.activeInHierarchy)
-                return bullet;
-        }
-        return null;
     }
 
     private IEnumerator CoolTime()
