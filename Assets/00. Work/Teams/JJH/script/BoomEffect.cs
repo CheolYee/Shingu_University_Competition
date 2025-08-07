aaaba5be22151e84d37ed68d51720b07f3dcb406
ForRest1217 <Jihan2519@naver.com>
2025-08-08 00:48:23 +0900

    BombEffect1

--- Assets/00. Work/Teams/JJH/script/BoomEffect.cs
+++ Assets/00. Work/Teams/JJH/script/BoomEffect.cs
@@ -1,5 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class BoomEffect : MonoBehaviour
 {
+    private void OnEnable()
+    {
+        StartCoroutine(Effect());
+    }
+
+    private IEnumerator Effect()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
 }
