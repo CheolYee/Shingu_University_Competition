ac68b7fe793fff53e9c193f318146f47b5f903d9
ForRest1217 <Jihan2519@naver.com>
2025-08-08 21:08:00 +0900

    EnemyAnimation

--- Assets/00. Work/02. Scripts/Systems/Enemy.cs
+++ Assets/00. Work/02. Scripts/Systems/Enemy.cs
@@ -7,27 +7,38 @@ public class Enemy : MonoBehaviour
     public event Action OnDead;
     private Bullet bullet;
     private Collider2D collid;
+    private Animator ani;
 
     private void Awake()
     {
         collid = GetComponent<Collider2D>();
+        ani = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
         bullet = GameObject.FindWithTag("Bullet").GetComponent<Bullet>();
     }
 
     private void Update()
     {
-        if (bullet.penetration)
-            collid.isTrigger = true;
-        else
-            collid.isTrigger = false;
+        if (bullet == null)
+        {
+            GameObject bullet = GameObject.FindWithTag("Bullet");
+            if (bullet != null)
+                this.bullet = bullet.GetComponent<Bullet>();
+            else
+                return;
+        }
+
+        collid.isTrigger = bullet.penetration;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            OnDead?.Invoke();
-            Destroy(gameObject);
+            ani.SetBool("Dead", true);
         }
     }
 
@@ -35,8 +46,13 @@ public class Enemy : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            OnDead?.Invoke();
-            Destroy(gameObject);
+            ani.SetBool("Dead", true);
         }
+    }
+
+    public void DeadAniEvent()
+    {
+        OnDead?.Invoke();
+        Destroy(gameObject);
     }
 }
