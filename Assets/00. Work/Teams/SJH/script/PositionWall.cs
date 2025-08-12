e29c8180359eabdcbc40046dadd39fd33b9f7a57
ForRest1217 <Jihan2519@naver.com>
2025-08-12 20:27:26 +0900

    PositionWallSelectRotation

--- Assets/00. Work/Teams/SJH/script/PositionWall.cs
+++ Assets/00. Work/Teams/SJH/script/PositionWall.cs
@@ -1,35 +1,51 @@
-using System.Collections.Generic;
-using UnityEngine;
 using DG.Tweening;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PositionWall : MonoBehaviour
 {
     [SerializeField] private List<Transform> location;
     [SerializeField] private float moveTime = 1f;
+    [SerializeField] private bool isRotation = false;
+    [SerializeField] private bool isRotationSelect = false;
+    [SerializeField] private float rotationSeclectValue = 0f;
+    [SerializeField] private float rotationSeclectSpeed = 0f;
+
     private int currentLocation = 0;
 
     void Start()
     {
-        StartCoroutine(MoveWall());
+        if (!isRotation && !isRotationSelect)
+            StartCoroutine(MoveWall());
+    }
+
+    private void Update()
+    {
+        if (isRotation && !isRotationSelect)
+            transform.Rotate(0f, 0f, 90f * Time.deltaTime);
+        else if (!isRotation && isRotationSelect)
+        {
+            float angle = Mathf.Sin(Time.time * rotationSeclectSpeed) * rotationSeclectValue;
+            transform.localRotation = Quaternion.Euler(0f, 0f, angle);
+        }
     }
 
     IEnumerator MoveWall()
     {
-        if(gameObject != null)
+        if (gameObject != null)
         {
-            transform.DOMove(location[currentLocation].position,moveTime).SetEase(Ease.Linear);
-            if(currentLocation == location.Count - 1)
+            transform.DOMove(location[currentLocation].position, moveTime).SetEase(Ease.Linear);
+            if (currentLocation == location.Count - 1)
             {
                 currentLocation = 0;
             }
-            else{
+            else
+            {
                 currentLocation++;
             }
             yield return new WaitForSeconds(moveTime);
             StartCoroutine(MoveWall());
         }
     }
-
-
 }
