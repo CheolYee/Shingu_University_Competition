f2ee290205cc8e38bad2514ab4761beec2253c41
CheolYee <mincheol2008@gmail.com>
2025-08-13 13:19:42 +0900

    Stage Select Scene Fixed
    Stage Select Scene Fixed

---
+++ Assets/00. Work/Teams/PMC/01. Codes/PlayerInputSo.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _00._Work.Teams.PMC._01._Codes
+{
+    [CreateAssetMenu(fileName = "PlayerInputSO", menuName = "SO/PlayerInputSo")]
+    public class PlayerInputSo : ScriptableObject, Controls.IPlayerActions
+    {
+        private Controls controls;
+
+        public event Action toggleMenu;
+        
+        private void OnEnable()
+        {
+            if (controls == null)
+            {
+                controls = new Controls();
+                controls.Player.SetCallbacks(this);
+            }
+            controls.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            controls.Player.Disable();
+        }
+
+        public void OnESC(InputAction.CallbackContext context)
+        {
+            toggleMenu?.Invoke();
+        }
+    }
+}
