dce3be292fd02647381b3b3932b317e116275cce
hongjihoo123 <hongjihoo123@naver.com>
2025-08-13 09:10:27 +0900

    Stage

--- Assets/00. Work/Teams/SJH/script/SwitchWall.cs
+++ Assets/00. Work/Teams/SJH/script/SwitchWall.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 
 public class SwitchWall : MonoBehaviour
 {
+    [SerializeField] private Vector2 boxSize = new Vector2(1f, 1f);
     [SerializeField] private GameObject switchGameObject;
     private Collider2D collider2d;
 
     void Update()
     {
         if(switchGameObject == null) return;
-        collider2d = Physics2D.OverlapBox(switchGameObject.transform.position, 
-        switchGameObject.transform.localScale, switchGameObject.transform.localRotation.z, 1<<6);
+        collider2d = Physics2D.OverlapBox(switchGameObject.transform.position,
+        boxSize, switchGameObject.transform.localRotation.z, 1<<6);
         if(collider2d != null)
         {
             Destroy(gameObject);
